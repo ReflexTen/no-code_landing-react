fda1f0aa5f23681d5726139f67a86aeb9d6768ad
ReflexTen <bulanov.roman4@gmail.com>
2023-05-10 21:50:21 +0500

    added adaptiv header and section home

---
+++ src/components/helpers/ReviewsData.js
@@ -0,0 +1,41 @@
+import clientOne from './../../images/reviews/client-1.jpg'
+
+const clients = [
+  {
+    idx: '1',
+    img: clientOne,
+    name: 'Rowhan Marlin',
+    review:
+      'Learning is a Global training provider based across the UK that specialises in accrediated and bespoke training courses.',
+  },
+  {
+    idx: '2',
+    img: clientOne,
+    name: 'Rowhan Marlin',
+    review:
+      'Learning is a Global training provider based across the UK that specialises in accrediated and bespoke training courses.',
+  },
+  {
+    idx: '3',
+    img: clientOne,
+    name: 'Rowhan Marlin',
+    review:
+      'Learning is a Global training provider based across the UK that specialises in accrediated and bespoke training courses.',
+  },
+  {
+    idx: '4',
+    img: clientOne,
+    name: 'Rowhan Marlin',
+    review:
+      'Learning is a Global training provider based across the UK that specialises in accrediated and bespoke training courses.',
+  },
+  {
+    idx: '5',
+    img: clientOne,
+    name: 'Rowhan Marlin',
+    review:
+      'Learning is a Global training provider based across the UK that specialises in accrediated and bespoke training courses.',
+  },
+]
+
+export default clients
---
+++ src/components/helpers/TeamData.js
@@ -0,0 +1,46 @@
+import peopleImg from './../../images/team/card-img.png'
+
+const membersTeam = [
+  {
+    idx: 1,
+    img: peopleImg,
+    name: 'Мария',
+    proffession: 'Co-Founder, CEO, продакт-менеджер',
+    about:
+      'Защищает интересы клиентов и переводит язык программистов на язык бизнеса',
+  },
+  {
+    idx: 2,
+    img: peopleImg,
+    name: 'Мария',
+    proffession: 'Co-Founder, CEO, продакт-менеджер',
+    about:
+      'Защищает интересы клиентов и переводит язык программистов на язык бизнеса',
+  },
+  {
+    idx: 3,
+    img: peopleImg,
+    name: 'Мария',
+    proffession: 'Co-Founder, CEO, продакт-менеджер',
+    about:
+      'Защищает интересы клиентов и переводит язык программистов на язык бизнеса',
+  },
+  {
+    idx: 4,
+    img: peopleImg,
+    name: 'Мария',
+    proffession: 'Co-Founder, CEO, продакт-менеджер',
+    about:
+      'Защищает интересы клиентов и переводит язык программистов на язык бизнеса',
+  },
+  {
+    idx: 5,
+    img: peopleImg,
+    name: 'Мария',
+    proffession: 'Co-Founder, CEO, продакт-менеджер',
+    about:
+      'Защищает интересы клиентов и переводит язык программистов на язык бизнеса',
+  },
+]
+
+export default membersTeam
